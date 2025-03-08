fb2af085c2ea3ed2aff005647354d55844890d5e
mrhapile <allinonegaming3456@gmail.com>
2025-03-08 10:12:00 +0530

    cpp

--- testpatter.cpp
+++ testpatter.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch='a';
+    for(int i=0;i<=5;i++){
+        for(int j=i;j>0;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
